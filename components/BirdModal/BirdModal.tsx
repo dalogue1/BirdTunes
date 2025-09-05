802cf81c681416c3141255211a14ac2c42ca3b04
dalogue1 <133064876+dalogue1@users.noreply.github.com>
2025-09-05 11:47:13 -0500

    Update BirdModal.tsx

--- components/BirdModal/BirdModal.tsx
+++ components/BirdModal/BirdModal.tsx
@@ -60,7 +60,7 @@ export function BirdModal({ data: bird, isOpen, onClose }: BirdModalProps) {
         <div className={styles.detections}>
           <Block bottom="3">
             <h2 className="h4">
-              {t("last")} {data?.length} {t("Detections")}
+              {t("last")} {data?.length} {t("detections")}
             </h2>
           </Block>
 
